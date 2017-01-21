012c9dd8c5bddb9e5f47960bcb98579b6d4989ec
Paul Gestwicki <pvgestwicki@bsu.edu>
2017-01-21 15:05:17 -0500

    Add fermata as player image

--- core/src/main/java/edu/bsu/ggj17/core/PlayerSprite.java
+++ core/src/main/java/edu/bsu/ggj17/core/PlayerSprite.java
@@ -1,20 +1,14 @@
 package edu.bsu.ggj17.core;
 
-import playn.core.Canvas;
 import playn.scene.ImageLayer;
-import playn.scene.Layer;
-import tripleplay.util.Colors;
 
 public class PlayerSprite {
 
     public final ImageLayer layer;
 
     public PlayerSprite(FlappyPitchGame game) {
-        Canvas canvas = game.plat.graphics().createCanvas(30,30);
-        canvas.setFillColor(Colors.BLACK);
-        canvas.fillRect(0,0,30,30);
-        layer = new ImageLayer(canvas.image);
-        layer.setOrigin(Layer.Origin.CENTER);
+        layer = new ImageLayer(game.plat.assets().getImage("images/fermata.png"));
+        layer.setOrigin(15,15);
     }
 
 }

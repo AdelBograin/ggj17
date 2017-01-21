93d9f8315f1a7bb653e02ac7c037c89bce363db7
Paul Gestwicki <pvgestwicki@bsu.edu>
2017-01-20 20:54:00 -0500

    Add TarsosDSP library
    Adding the source was, it turned out, the easiest solution, since there's no maven dependency available for Tarsos.

--- core/src/main/java/edu/bsu/ggj17/core/FlappyPitchGame.java
+++ core/src/main/java/edu/bsu/ggj17/core/FlappyPitchGame.java
@@ -1,5 +1,6 @@
 package edu.bsu.ggj17.core;
 
+import be.tarsos.dsp.pitch.PitchDetectionHandler;
 import playn.core.Image;
 import playn.core.Platform;
 import playn.scene.ImageLayer;
@@ -7,14 +8,16 @@ import playn.scene.SceneGame;
 
 public class FlappyPitchGame extends SceneGame {
 
-  public FlappyPitchGame (Platform plat) {
-    super(plat, 33); // update our "simulation" 33ms (30 times per second)
+    public FlappyPitchGame(Platform plat) {
+        super(plat, 33); // update our "simulation" 33ms (30 times per second)
 
-    // create and add background image layer
-    Image bgImage = plat.assets().getImage("images/bg.png");
-    ImageLayer bgLayer = new ImageLayer(bgImage);
-    // scale the background to fill the screen
-    bgLayer.setSize(plat.graphics().viewSize);
-    rootLayer.add(bgLayer);
-  }
+        // create and add background image layer
+        Image bgImage = plat.assets().getImage("images/bg.png");
+        ImageLayer bgLayer = new ImageLayer(bgImage);
+        // scale the background to fill the screen
+        bgLayer.setSize(plat.graphics().viewSize);
+        rootLayer.add(bgLayer);
+
+        PitchDetectionHandler pitch;
+    }
 }

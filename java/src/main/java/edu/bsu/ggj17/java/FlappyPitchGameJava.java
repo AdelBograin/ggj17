0d70c019af183e062072028aefcd29da34e5de05
Paul Gestwicki <pvgestwicki@bsu.edu>
2017-01-21 15:18:49 -0500

    Increase game size to 800x600

--- java/src/main/java/edu/bsu/ggj17/java/FlappyPitchGameJava.java
+++ java/src/main/java/edu/bsu/ggj17/java/FlappyPitchGameJava.java
@@ -6,11 +6,12 @@ import edu.bsu.ggj17.core.FlappyPitchGame;
 
 public class FlappyPitchGameJava {
 
-  public static void main (String[] args) {
-    LWJGLPlatform.Config config = new LWJGLPlatform.Config();
-    // use config to customize the Java platform, if needed
-    LWJGLPlatform plat = new LWJGLPlatform(config);
-    new FlappyPitchGame(plat);
-    plat.start();
-  }
+    public static void main(String[] args) {
+        LWJGLPlatform.Config config = new LWJGLPlatform.Config();
+        config.width = 800;
+        config.height = 600;
+        LWJGLPlatform plat = new LWJGLPlatform(config);
+        new FlappyPitchGame(plat);
+        plat.start();
+    }
 }

1ba36d983e3437283aa0177c3b3b38b6abd9b75a
Paul Gestwicki <pvgestwicki@bsu.edu>
2017-01-21 22:02:32 -0500

    Clean up the layout

--- core/src/main/java/edu/bsu/ggj17/core/MenuScreen.java
+++ core/src/main/java/edu/bsu/ggj17/core/MenuScreen.java
@@ -54,56 +54,57 @@ public class MenuScreen extends ScreenStack.UIScreen {
             }
         });
 
-
-        Root root = iface.createRoot(AxisLayout.vertical(), GameStyles.newSheet(game.plat.graphics()), layer)
-                .setSize(game.plat.graphics().viewSize);
-
-        Font font = new Font("Bold", 40);
-        Font smaller = new Font("Bold", 22);
-
-        root.add(new Label("Fermata").setStyles(Style.COLOR.is(Colors.WHITE), Style.FONT.is(font)),
-                new Label("or").setStyles(Style.COLOR.is(Colors.WHITE), Style.FONT.is(smaller)),
-                new Label("Don't Breathe").setStyles(Style.COLOR.is(Colors.WHITE), Style.FONT.is(font)),
-                new Shim(12,12),
-                new Button("Play").onClick(new UnitSlot() {
-                    @Override
-                    public void onEmit() {
-                        startNewGame(true);
-                    }
-
-                    private void startNewGame(boolean first) {
-                        GameScreen gameScreen = new GameScreen(game);
-                        gameScreen.done.connect(new Slot<EndOption>() {
+        Group buttonGroup = new Group(AxisLayout.vertical().gap(12).offEqualize())
+                .add(new Button("Play").onClick(new UnitSlot() {
                             @Override
-                            public void onEmit(EndOption endOption) {
-                                if (endOption == EndOption.PLAY_AGAIN) {
-                                    startNewGame(false);
-                                }
-                                else {
-                                    screenStack.popTo(MenuScreen.this);
+                            public void onEmit() {
+                                startNewGame(true);
+                            }
+
+                            private void startNewGame(boolean first) {
+                                GameScreen gameScreen = new GameScreen(game);
+                                gameScreen.done.connect(new Slot<EndOption>() {
+                                    @Override
+                                    public void onEmit(EndOption endOption) {
+                                        if (endOption == EndOption.PLAY_AGAIN) {
+                                            startNewGame(false);
+                                        } else {
+                                            screenStack.popTo(MenuScreen.this);
+                                        }
+                                    }
+                                });
+                                if (first) {
+                                    screenStack.push(gameScreen, new FadeTransition().duration(500));
+                                } else {
+                                    screenStack.replace(gameScreen, new FadeTransition().duration(500));
                                 }
                             }
-                        });
-                        if (first) {
-                            screenStack.push(gameScreen, new FadeTransition().duration(500));
-                        } else {
-                            screenStack.replace(gameScreen, new FadeTransition().duration(500));
-                        }
-                    }
-                }),
-                new Button("About").onClick(new UnitSlot() {
-                    @Override
-                    public void onEmit() {
-                        screenStack.push(aboutScreen, screenStack.slide().left());
-                    }
-                }),
-                new Button("Configure").onClick(new UnitSlot() {
-                    @Override
-                    public void onEmit() {
-                        screenStack.push(configScreen, screenStack.slide().left());
-                    }
-                })
-        );
+                        }),
+                        new Button("About").onClick(new UnitSlot() {
+                            @Override
+                            public void onEmit() {
+                                screenStack.push(aboutScreen, screenStack.slide().left());
+                            }
+                        }),
+                        new Button("Configure").onClick(new UnitSlot() {
+                            @Override
+                            public void onEmit() {
+                                screenStack.push(configScreen, screenStack.slide().left());
+                            }
+                        }));
+
+
+        Root root = iface.createRoot(AxisLayout.vertical().offEqualize(), GameStyles.newSheet(game.plat.graphics()), layer)
+                .setSize(game.plat.graphics().viewSize);
+
+        Styles bigFontStyles = Styles.make(Style.COLOR.is(Colors.WHITE), Style.FONT.is(new Font("Bold", 40)));
+        Styles smallFontStyles = Styles.make(Style.COLOR.is(Colors.WHITE), Style.FONT.is(new Font("Bold", 28)));
+
+        root.add(new Label("Fermata").setStyles(bigFontStyles),
+                new Label("or").setStyles(smallFontStyles),
+                new Label("Don't Breathe").setStyles(bigFontStyles),
+                new Shim(24, 24),
+                buttonGroup);
     }
 
     @Override
